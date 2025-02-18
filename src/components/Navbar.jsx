a28cad9e49ba739b34aa62d52c2ab8b9053bf3a1
Brayarn Ogeda <brayarnogeda@gmail.com>
2025-02-18 22:43:26 +0300

    modify NavLinks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
+import { HiOutlineShoppingBag } from "react-icons/hi";
+
+const NavLinks = () => {
+  return (
+    <ul className="flex justify-around items-center gap-8">
+      <li>Home</li>
+      <li>Shop</li>
+      <li>Blog</li>
+      <li>About</li>
+      <li>Contact</li>
+      <li>
+        <button className="cursor-pointer">
+          <HiOutlineShoppingBag size={20} />
+        </button>
+      </li>
+    </ul>
+  );
+};
 const Navbar = () => {
   return (
     <nav>
-      <div className="md:ms-20 ms-10 md:me-20 me-8 py-4 flex justify-between">
-        <h1>Freshtyles</h1>
-        <ul className="flex justify-around gap-8">
-          <li>Home</li>
-          <li>Shop</li>
-          <li>Blog</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li></li>
-        </ul>
+      <div className="md:ms-20 ms-10 md:me-24 me-8 py-4 flex justify-between">
+        <h1 className="font-semibold text-2xl">Freshtyles</h1>
+
+        <NavLinks />
       </div>
     </nav>
   );
